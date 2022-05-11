94775d5e06ba2675e0fef00a8db88e0ed7d73d4d
jun0705 <105354964+jun0705@users.noreply.github.com>
2022-05-11 22:16:02 +0900

    Add files via upload

---
+++ file/static/jun.js
@@ -0,0 +1,41 @@
+var sw = document.getElementById("sw");
+sw.addEventListener('click',function()
+{ 
+    alert ("speaking & writing");
+});
+
+var lr = document.getElementById("LR");
+lr.addEventListener('click',function()
+{ 
+    alert ("listening & reading");
+});
+
+var computer = document.getElementById("computer");
+computer.addEventListener('click',function()
+{ 
+    alert ("컴퓨터기초");
+});
+
+var cprograming = document.getElementById("cprograming");
+cprograming.addEventListener('click',function()
+{ 
+    alert ("C 프로그래밍");
+});
+
+var writing = document.getElementById("writing");
+writing.addEventListener('click',function()
+{ 
+    alert ("글쓰기기초");
+});
+
+var Math = document.getElementById("Math");
+Math.addEventListener('click',function()
+{ 
+    alert ("확률통계");
+});
+
+var Information = document.getElementById("Information");
+Information.addEventListener('click',function()
+{ 
+    alert ("정보사화와 소통");
+});
